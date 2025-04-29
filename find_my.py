b7a949af13eefc1c1485f888a6eb2d270f54860d
Aaron M. Watson <watsona4@gmail.com>
2025-04-29 07:18:57 -0400

    ✨ feat: add support for extracting and processing Apple Find My devices data
    This commit introduces support for extracting and processing Apple Find My devices data, including downloading Devices.data and Items.data from iOS devices. The extracted data is processed into JSON objects, with additional logging and MQTT data publication functionalities for each device.

--- find_my.py
+++ find_my.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+import os.path
 import time
 from datetime import datetime, timezone
 from zoneinfo import ZoneInfo
@@ -18,7 +19,8 @@ IOS_URL: str = str(os.environ.get("IOS_URL", ""))
 IOS_USERNAME: str = "root"
 IOS_KEYFILE: str = "/ssh/id_rsa"
 KNOWN_HOSTS: str = "/ssh/known_hosts"
-IOS_DATAPATH: str = "/private/var/mobile/Library/Caches/com.apple.findmy.fmipcore/Items.data"
+IOS_DEVICESPATH: str = "/private/var/mobile/Library/Caches/com.apple.findmy.fmipcore/Devices.data"
+IOS_ITEMSPATH: str = "/private/var/mobile/Library/Caches/com.apple.findmy.fmipcore/Items.data"
 
 logging.basicConfig(format="%(asctime)s [%(levelname)s]: %(message)s", level=logging.INFO)
 
@@ -56,12 +58,15 @@ def main():
         sftp = ssh_client.open_sftp()
 
         logging.info("Downloading Items.data from iOS device")
-        sftp.get(remotepath=IOS_DATAPATH, localpath="Items.data")
+        sftp.get(remotepath=IOS_ITEMSPATH, localpath=os.path.basename(IOS_ITEMSPATH))
+
+        logging.info("Downloading Devices.data from iOS device")
+        sftp.get(remotepath=IOS_DEVICESPATH, localpath=os.path.basename(IOS_DEVICESPATH))
 
         sftp.close()
 
-        logging.info("Extracting data into JSON object")
-        with open("Items.data") as datafile:
+        logging.info("Extracting items data into JSON object")
+        with open(os.path.basename(IOS_ITEMSPATH)) as datafile:
             data = json.load(datafile)
 
         logging.info("Number of Apple Find My objects to process: %d", len(data))
@@ -69,7 +74,6 @@ def main():
 
             logging.info("Gathering data for next Apple Find My object to process")
 
-            logging.info("Data gathered, sending to MQTT broker %s", MQTT_HOST)
             identifier = obj["identifier"]
             name = obj["name"]
             manufacturer = obj["productType"]["productInformation"]["manufacturerName"]
@@ -126,6 +130,84 @@ def main():
             }
 
             logging.info("Sending MQTT data of Apple Find My object: %s", name)
+            logging.info("    Address: %s", address)
+
+            mqtt_client.publish(config_topic, json.dumps(config_data), retain=True)
+            mqtt_client.publish(state_topic, state, retain=True)
+            mqtt_client.publish(attributes_topic, json.dumps(attributes), retain=True)
+            mqtt_client.publish(data_topic, json.dumps(obj), retain=True)
+
+        logging.info("Extracting devices data into JSON object")
+        with open(os.path.basename(IOS_DEVICESPATH)) as datafile:
+            data = json.load(datafile)
+
+        logging.info("Number of Apple Find My devices to process: %d", len(data))
+        for obj in data:
+
+            logging.info("Gathering data for next Apple Find My device to process")
+
+            location_capable = obj["locationCapable"]
+            if not location_capable:
+                continue
+            
+            location = obj["location"]
+            if location is None:
+                continue
+
+            identifier = obj["deviceDiscoveryId"]
+            name = obj["name"]
+            manufacturer = obj["deviceClass"]
+            model = obj["deviceModel"]
+            serial_number = obj["deviceDiscoveryId"]
+            sw_version = obj["deviceDisplayName"]
+
+            address = obj["address"]["mapItemFullAddress"]
+
+            latitude = obj["location"]["latitude"]
+            longitude = obj["location"]["longitude"]
+            altitude = obj["location"]["altitude"]
+            vertical_accuracy = obj["location"]["verticalAccuracy"]
+            horizontal_accuracy = obj["location"]["horizontalAccuracy"]
+            timestamp = datetime.fromtimestamp(
+                int(obj["location"]["timeStamp"]) // 1000, timezone.utc
+            ).astimezone(ZoneInfo(TZ))
+
+            battery_status = obj["batteryStatus"]
+
+            topic_base = f"homeassistant/device_tracker/findmy_{serial_number}/"
+
+            config_topic = topic_base + "config"
+            state_topic = topic_base + "state"
+            attributes_topic = topic_base + "attributes"
+            data_topic = topic_base + "data"
+
+            config_data = {
+                "unique_id": identifier,
+                "name": "Tracker",
+                "state_topic": state_topic,
+                "json_attributes_topic": attributes_topic,
+                "device": {
+                    "manufacturer": manufacturer,
+                    "model_id": model,
+                    "identifiers": serial_number,
+                    "name": name,
+                    "sw_version": sw_version,
+                },
+            }
+
+            state = "None"
+
+            attributes = {
+                "latitude": latitude,
+                "longitude": longitude,
+                "altitude": altitude,
+                "vertical_accuracy": vertical_accuracy,
+                "gps_accuracy": horizontal_accuracy,
+                "battery_status": battery_status,
+                "timestamp": timestamp.strftime("%c"),
+            }
+
+            logging.info("Sending MQTT data of Apple Find My device: %s", name)
             logging.info("    Address: %s", address)
 
             mqtt_client.publish(config_topic, json.dumps(config_data), retain=True)
